89657a66ab474f0a77f7e266615d78960c0d611a
tonyekim <otoobongekim@gmail.com>
2024-03-06 16:18:17 +0100

    create the about page

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 const Providers = ({children}) => {
   return (
     <ThemeProvider defaultTheme='system' attribute='class'>
-        <div>
+        <div className=' text-gray-700 dark:text-gray-200 select-none min-h-screen transition-colors duration-300'>
             {children}
         </div>
     </ThemeProvider>
