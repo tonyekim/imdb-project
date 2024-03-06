a67e554af21b24bfc8329bcc33197de6aa0bcd05
tonyekim <otoobongekim@gmail.com>
2024-03-06 22:01:45 +0100

    add loading effect using nextjs

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className='flex justify-center mt-16'>
+      <img className='h-96' src="/Spinner.svg" alt="loading..." />
+    </div>
+  );
+};
+
+export default Loading;
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,19 @@
 import Result from "@/components/Result";
-import React from "react";
+
 const API_KEY = process.env.API_KEY;
 
-const Home = async ({ searchParams }) => {
-  const genre = searchParams.genre || "fetchTrending";
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
   const res = await fetch(
     `https://api.themoviedb.org/3${
-      genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
   );
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
-  }
   const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
   const results = data.results;
 
   return (
@@ -20,6 +21,4 @@ const Home = async ({ searchParams }) => {
       <Result results={results} />
     </div>
   );
-};
-
-export default Home;
+}
