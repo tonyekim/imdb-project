c41cf9bcc65c27ee523b4437ed248f2ced191b98
tonyekim <otoobongekim@gmail.com>
2024-03-07 12:17:05 +0100

    fix error component

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,13 +1,16 @@
 "use client";
-import React, { useEffect } from "react";
 
-export default function error({ error, reset }) {
-  useEffect(() => {}, [error]);
-
+import { useEffect } from "react";
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
   return (
-    <div className=" text-center mt-10">
-      <h2>Something went wrong please try again</h2>
-      <button className=" hover:text-amber-600" onClick={() => reset()}>Try again</button>
+    <div className="text-center mt-10">
+      <h1>Something went wrong. Please try again later.</h1>
+      <button className="hover:text-amber-600" onClick={() => reset()}>
+        Try Again
+      </button>
     </div>
   );
 }
